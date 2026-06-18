36d03fae070cacd9c0a2b36e6a50e41b55204ba7
Son Le V <son.lev@one-line.com>
2026-06-18 23:09:11 +0700

    chore: remove unused web package.json and assets (book.ico, book.png)

--- public/config/config.js
+++
@@ -1,6 +0,0 @@
-window.TRANSLATOR_CONFIG = {
-  defaultProvider: "openrouter",
-  defaultModel: "mistralai/mistral-nemo",
-  // Public config only. Do NOT put secrets in this file.
-  keys: {},
-};
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { bootstrapApp } from "./bootstrap.js";
-
-bootstrapApp().catch(function (error) {
-  console.error("App bootstrap failed:", error);
-});
-
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
